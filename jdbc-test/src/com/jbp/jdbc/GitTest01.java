f119d2e720df01ea4862dc9e0a0e84c30770fa5a
JXP <jixuanpu2016@outlook.com>
2021-01-31 16:34:52 +0800

    test insert by PC

--- jdbc-test/src/com/jbp/jdbc/GitTest01.java
+++ jdbc-test/src/com/jbp/jdbc/GitTest01.java
@@ -3,5 +3,6 @@ package com.jbp.jdbc;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("IDEA Git Test！");
+        System.out.println("insert by PC");
     }
 }
